3a0e96f2f772245147ca07731464114cf1063484
to4to <justjustifying@outlook.com>
2024-12-09 23:52:50 +0530

    Initial Commit

---
+++ FitFlow/FitFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitFlowApp.swift
+//  FitFlow
+//
+//  Created by Himanshu KumarSingh on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FitFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitFlowTests/FitFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitFlowTests.swift
+//  FitFlowTests
+//
+//  Created by Himanshu KumarSingh on 09/12/24.
+//
+
+import Testing
+@testable import FitFlow
+
+struct FitFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
